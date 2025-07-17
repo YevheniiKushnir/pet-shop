6e7bcb3e1b33e89d36a39001d1c6b28352a5a08a
Yevhenii Kushnir <aroblack17@gmail.com>
2025-07-17 21:52:35 +0200

    changed api frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3333";
+export const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3333"
+    : "https://pet-shop-qfw8.onrender.com";
 
 const API = axios.create({
   baseURL: BASE_URL,
